b6bbd68dfa4e807a4dfef69cdd9843355543a1bf
DivineCross <DivineCross@users.noreply.github.com>
2026-01-10 20:41:20 +0800

    Create folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>Guild Loot</h1>
+            <div>Record all loot items!</div>
+        </>
+    )
+}
+
+export default App
