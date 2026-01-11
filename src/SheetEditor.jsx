297c824c1c330747b17b8daa512fcd3bdd00ec27
DivineCross <DivineCross@users.noreply.github.com>
2026-01-12 02:19:39 +0800

    Implement SheetEditor

--- src/SheetEditor.jsx
+++ src/SheetEditor.jsx
@@ -1,6 +1,46 @@
+import { transactionHeads, transactionRows } from './mock';
+
+// TODO: repalce with well-defined structure
+const heads = transactionHeads;
+const rows = transactionRows;
+
 export default function SheetEditor() {
+    const gridStyle = { gridTemplateColumns: `repeat(${heads.length}, max-content)` };
+
     return (
-        <main className="sheet-editor">
-        </main>
+        <div className="sheet-editor" style={gridStyle}>
+            <SheetHead />
+            <SheetBody />
+        </div>
     );
 };
+
+function SheetHead() {
+    return (
+        <SheetRow cells={heads} isHead={true} />
+    );
+}
+
+function SheetBody() {
+    return rows.map((row, i) =>
+        <SheetRow key={i} cells={row} />
+    );
+}
+
+function SheetRow({ cells = [], isHead = false }) {
+    const headClass = isHead ? ' sheet__head' : '';
+
+    return (
+        <div className={`sheet__row${headClass}`}>{cells.map((cell, i) =>
+            <SheetCell key={i} value={cell} />
+        )}</div>
+    );
+}
+
+function SheetCell({ value }) {
+    return (
+        <div className="sheet__cell">
+            {value}
+        </div>
+    );
+}
