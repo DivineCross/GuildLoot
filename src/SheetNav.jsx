99896f0807b2205a732cf9c50833b86f5fa43304
DivineCross <DivineCross@users.noreply.github.com>
2026-01-12 16:27:11 +0800

    Add props to SheetNav

--- src/SheetNav.jsx
+++ src/SheetNav.jsx
@@ -1,26 +1,23 @@
-import { useState } from 'react';
-import { sheetNames } from './mock';
-
-export default function SheetNav() {
-    const [activeIndex, setActiveIndex] = useState(0);
-
+export default function SheetNav({ names = [], activeName = '', onSelect }) {
     return (
-        <div className="sheet-nav">{sheetNames.map((x, i) =>
+        <div className="sheet-nav">{names.map((name, i) =>
             <SheetNavItem
                 key={i}
-                label={x}
-                isActive={i == activeIndex}
-                onClick={() => setActiveIndex(i)} />
+                name={name}
+                isActive={name == activeName}
+                onSelect={onSelect} />
         )}</div>
     );
 };
 
-function SheetNavItem({ label, isActive, onClick }) {
+function SheetNavItem({ name = '', isActive = false, onSelect }) {
     const activeClass = isActive ? ' sheet-nav__item--active' : '';
 
     return (
-        <div className={`sheet-nav__item${activeClass}`} onClick={onClick}>
-            <span>{label}</span>
+        <div
+            className={`sheet-nav__item${activeClass}`}
+            onClick={() => onSelect(name)}>
+            <span>{name}</span>
         </div>
     );
 }
