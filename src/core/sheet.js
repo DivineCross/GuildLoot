8af19089ead09a9e8caf32a30606865c6bc91236
DivineCross <DivineCross@users.noreply.github.com>
2026-01-16 22:12:59 +0800

    Add sheet properties and methods

--- src/core/sheet.js
+++ src/core/sheet.js
@@ -1,8 +1,19 @@
 class Sheet {
     constructor(name = '', heads = [], rows = []) {
+        /** @type {string} */
         this.name = name;
+        /** @type {Cell[]} */
         this.heads = heads;
+        /** @type {Cell[][]} */
         this.rows = rows;
+    }
+
+    get rowCount() {
+        return this.rows.length;
+    }
+
+    get colCount() {
+        return this.heads.length;
     }
 
     static fromObject({ name = '', heads = [], rows = [] } = {}) {
@@ -18,11 +29,31 @@ class Sheet {
 
         return new Sheet(name, rows[0], rows.slice(1));
     }
+
+    normalize() {
+        this.rows = this.rows.map(row =>
+            [...Array(this.colCount).keys()].map(i => row[i] ?? new Cell));
+
+        for (let i = this.rowCount - 1; i >= 0; --i)
+            if (this.rows[i].every(c => c.isEmpty))
+                this.rows.pop();
+            else
+                break;
+    }
+
+    addRow() {
+        const newRow = [...Array(this.colCount)].map(() => new Cell);
+        this.rows.push(newRow);
+    }
 }
 
 class Cell {
     constructor(value = '') {
         this.value = value;
+    }
+
+    get isEmpty() {
+        return !this.value.trim().length;
     }
 
     static fromObject({ value = '' }) {
