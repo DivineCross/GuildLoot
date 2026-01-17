e9ad9d703d694a26e1b89070df85dc897dc72838
DivineCross <DivineCross@users.noreply.github.com>
2026-01-17 18:31:08 +0800

    Add validation logic

---
+++ src/core/sheetMeta.js
@@ -0,0 +1,29 @@
+import Validator from './validator';
+import Sheet from './sheet';
+
+/** @param {Map<string, Sheet>} sheetMap */
+function CreateValidator(sheetMap) {
+    sheetMap.get('戰利品').colValidators = [
+        null,
+        Validator.FromSheet(sheetMap.get('頭目')),
+        Validator.FromSheet(sheetMap.get('成員')),
+        Validator.FromSheet(sheetMap.get('道具')),
+        null,
+        Validator.FromSheet(sheetMap.get('成員')),
+        null,
+        null,
+    ];
+    sheetMap.get('交易所').colValidators = [
+        null,
+        Validator.FromSheet(sheetMap.get('道具')),
+        null,
+        null,
+        Validator.FromSheet(sheetMap.get('成員')),
+        null,
+    ];
+    sheetMap.get('口袋').colValidators = [
+        Validator.FromSheet(sheetMap.get('道具')),
+    ];
+}
+
+export { CreateValidator as SetSheetMeta };
