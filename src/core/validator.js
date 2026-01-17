c938cd0cb05ac406bad07f9a35684b9dfc39dbcc
DivineCross <DivineCross@users.noreply.github.com>
2026-01-17 20:09:36 +0800

    Add integer validation

--- src/core/validator.js
+++ src/core/validator.js
@@ -1,22 +1,66 @@
 import Sheet, { Cell } from './sheet';
 
 export default class Validator {
-    constructor(validValues = []) {
-        this.validValues = validValues;
+    constructor() {
+        /** @type {string[] | undefined} */
+        this.validValues = undefined;
+        /** @type {number | undefined} */
+        this.min = undefined;
+        /** @type {number | undefined} */
+        this.max = undefined;
     }
 
     get message() {
         return '違反驗證規則';
     }
 
+    get isByValues() {
+        return [this.validValues].every(x => x !== undefined);
+    }
+
+    get isByIntMinMax() {
+        return [this.min, this.max].every(x => x !== undefined);
+    }
+
     /** @param {Sheet} sheet */
     static FromSheet(sheet) {
-        return new Validator(sheet.rows.flatMap(
-            row => row.filter(c => !c.isEmpty).map(c => c.value)));
+        const v = new Validator;
+        v.validValues = sheet.rows.flatMap(
+            row => row.filter(c => !c.isEmpty).map(c => c.value));
+
+        return v;
+    }
+
+    static FromIntMinMax(min, max) {
+        if (!(Number.isSafeInteger(min) && Number.isSafeInteger(max)))
+            throw new Error('min and max should be integer');
+        if (min > max)
+            throw new Error('min should be less than max');
+
+        const v = new Validator;
+        v.min = min;
+        v.max = max;
+
+        return v;
     }
 
     /** @param {Cell} cell */
     validate(cell) {
-        return cell.isEmpty || this.validValues.includes(cell.value);
+        if (cell.isEmpty)
+            return true;
+
+        if (this.isByValues)
+            return this.validValues.includes(cell.value);
+
+        if (this.isByIntMinMax) {
+            const str = cell.value;
+            const num = /^(?:(?:-?[1-9]\d*)|0)$/.test(str) ? Number(str) : null;
+
+            return (num === null || !Number.isSafeInteger(num))
+                ? false
+                : this.min <= num && num <= this.max;
+        }
+
+        return false;
     }
 }
