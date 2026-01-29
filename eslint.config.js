b94e7bf85ece4b404401db72a5592c6dd841cc4a
DivineCross <DivineCross@users.noreply.github.com>
2026-01-30 00:51:40 +0800

    Fix eslint config for ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint'
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -19,7 +20,7 @@ export default defineConfig([
             globals: globals.browser,
         },
         rules: {
-            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            '@typescript-eslint/no-unused-vars': ['error'],
             'indent': ['error', 4, { SwitchCase: 1 }],
             'semi': ['error', 'always'],
             'quotes': ['warn', 'single'],
